91edee5fcc95d868f4d63bc97a3dde59b69a96a9
Stefan <stefan.franke@itc-studenten.de>
2023-06-06 19:43:20 +0200

    homepage

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div class="header">
+  <div class="header">
+    <div >
       <v-row style="height: 70px">
         <v-col cols="4">
           <v-img
@@ -13,26 +13,26 @@
         <v-col cols="4">
           <h1 class="text-white mt-5">Bürgergemeinschaft Werl</h1>
         </v-col>
-        <v-col cols="4">
-          <v-btn class="mt-10 mitglied-button">
+        <v-col class="d-flex justify-center " cols="4">
+          <v-btn class="mr-3 mt-10 mitglied-button">
             Mitglied werden
           </v-btn>
         </v-col>
       </v-row>
 
       <div class="d-flex justify-center" style="width: 100%;">
-        <v-row class="mt-9 text-white" style="width: 50%;">
-          <v-col cols="1">
+        <v-row class="mt-9 d-flex justify-center text-white" style=" width: 100%;">
+          <v-col class="nav-cols" cols="1">
             <p class="">
               Unser Team
             </p>
           </v-col>
-          <v-col cols="1">Aktuelles</v-col>
-          <v-col cols="1">BG-Position</v-col>
-          <v-col cols="1">BG-Erfolge</v-col>
-          <v-col cols="1">Kommentare</v-col>
-          <v-col cols="1">Pressearchiv</v-col>
-          <v-col cols="1">Tagesordung</v-col>
+          <v-col class="nav-cols"  cols="1">Aktuelles</v-col>
+          <v-col class="nav-cols" cols="1">BG-Position</v-col>
+          <v-col class="nav-cols" cols="1">BG-Erfolge</v-col>
+          <v-col class="nav-cols" cols="1">Kommentare</v-col>
+          <v-col class="nav-cols" cols="1">Pressearchiv</v-col>
+          <v-col class="nav-cols" cols="1">Tagesordung</v-col>
 
         </v-row>
       </div>
@@ -56,10 +56,15 @@ export default {
 .header {
   height: 150px;
   background: #2F53A7;
+  box-shadow: 0px 3px 1px black;
 }
 
 .mitglied-button {
   color: white;
   background: blue;
+}
+.nav-cols{
+  min-width: 140px;
+
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,28 @@
 <template>
   <div>
-    <div>
-      <HeaderComponent/>
-    </div>
 
-    <div class="background"/>
+    <HeaderComponent></HeaderComponent>
+
+    <div class="background pt-10">
+      <v-row  class="d-flex justify-center">
+        <v-col class="d-flex justify-center" cols="7">
+          <v-card class=" card-links">
+            <h2 class="px-10 pt-3 text-center">Herzlich Willkommen auf den
+              Seiten der Bürgergemeinschaft Werl e.V.!
+            </h2>
+            <p class="px-5 pt-15 text-center">
+              Die Bürgergemeinschaft Werl e.V. (BG-Werl) ist ein Zusammenschluss parteiunabhängiger Bürgerinnen und Bürger, die auf kommunaler Ebene bürgernahe politische Arbeit leisten. Die BG ist eine ideologiefreie kommunale Wählergemeinschaft, die alle Fragen und Probleme unserer Stadt in ständiger Diskussion gemeinsam mit und für die Bürgerinnen und Bürger zu lösen versucht. Wir entscheiden frei von Weisungen und Programmen eines Landes- oder Bundesverbandes und orientieren uns allein am Wohl der Stadt. Die Mandatsträger entscheiden frei nach ihrem Gewissen und unterliegen keinem Fraktionszwang.
+            </p>
+          </v-card>
+        </v-col>
+        <v-col class="d-flex justify-center" cols="5">
+          <v-card class="card-links">
+            <v-img cover :src="bildRechts"></v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+
+    </div>
   </div>
 
 
@@ -16,7 +34,11 @@ import HeaderComponent from "@/components/HeaderComponent";
 
 export default ({
   name: 'HomeView',
-
+  data(){
+    return{
+      bildRechts: require('../assets/vorstandneu.jpeg')
+    }
+  },
   components: {
     HeaderComponent
   },
@@ -27,12 +49,18 @@ export default ({
 
 .background {
   width: 100vw;
-  height: 89vh;
-  background-image: url('/Users/lgraf/Library/CloudStorage/OneDrive-QuinScapeGmbH/PHPStorm/burgergemeinschaft-werl/src/assets/werl.png');
+  height: 81vh;
+  background-image: url('../assets/werl.png');
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
   background-attachment: fixed;
 
+}
+.card-links{
+  height: 70vh;
+  width: 90%;
+  background-color: rgba(255, 255, 255, 0.8);
+  box-shadow: 4px 6px 8px black;
 }
 </style>
