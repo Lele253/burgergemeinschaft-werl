566c6bc87f27567497e063cfa4ae723190cdf8d6
Lele253 <120642684+Lele253@users.noreply.github.com>
2023-06-12 09:31:24 +0200

    array in den store / ErfolgeView.vue redundanz entfernt

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,172 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
 
 export default createStore({
-  state: {
-    user: false,
-    routername: 'BG Werl',
-  },
-  getters: {
-    user: (state) => {
-      return state.user
-    }
-  },
-  mutations: {
-    user(state, user) {
-      state.user = user
-    }
-  },
-  actions: {
-    user(context, user) {
-      context.commit('user', user)
-    }
-  },
-  modules: {
-  }
+    state: {
+        user: false,
+        routername: 'BG Werl',
+        beiträge: [
+            {
+                "id": 1,
+                "titel": "Stellungnahme der BG Werl zur Diskussion über Lärmbelastung bei Schützenfesten",
+                "text": "SCHÜTZENFESTE - ERNEUT RECHTSUNSICHERHEIT BEI DER VERWALTUNG ?\n" +
+                    "\n" +
+                    " \n" +
+                    "\n" +
+                    "In der letzten Ratssitzung wurde die Verwaltung nach Einschränkungen der Lärmbelästigung durch Schützenfeste gefragt.\n" +
+                    "\n" +
+                    " \n" +
+                    "\n" +
+                    "Seitens der Stadt Werl wurde betont, dass nach Beschwerden durch Anlieger die gesetzlichen Vorgaben einzuhalten seien, nämlich 45 Dezibel nach 22 Uhr. Das ist eindeutig falsch! Uns liegt eine Stellungnahme eines führenden Verwaltungsrechtlers vor, mit dem auch die Stadt Werl gelegentlich arbeitet.\n" +
+                    "\n" +
+                    "Das Oberverwaltungsgericht Münster hat folgendes bestimmt: Erst ab 0.00 Uhr ist sicherzustellen, dass ein Pegel von 55 dB vor dem Wohnhaus der Anwohner nicht überschritten wird. Das bedeutet, der Verstärker muss so eingestellt werden, dass drei Meter vor den Lautsprechern maximal 80 dB erzeugt werden. Die musikalischen Darbietungen sind um 1.45 Uhr einzustellen und das Fest muss um 2.00 Uhr beendet werden.\n" +
+                    "\n" +
+                    "Diese Informationen kann man auch im Internet nachlesen. Warum informiert sich die Verwaltung hier nicht fundierter? Und auch der Sönneraner UWG Ratsherr und Schützenbruder Prünte hatte sich hier als Anfragender im Vorfeld offenbar nur unzureichend informiert, ansonsten hätte er jede Möglichkeit gehabt, dem Vortrag der Stadt direkt zu widersprechen.\n" +
+                    "\n" +
+                    " \n" +
+                    "\n" +
+                    "Wir haben wirklich Verständnis für die Bürger, die irgendwann zur Ruhe kommen möchten. Aber Schützenfest ist einmal pro Jahr und die oben beschriebenen Richtwerte sind vertretbar.",
+                "datum": "01.06.2023"
+            },
+            {
+                "id": 2,
+                "titel": "Stellungnahme zum Umbesetzungsantrag der UWG-Fraktion",
+                "text": "Die neue UWG-Fraktion im Werler Rat, bestehend aus den ehemaligen Ratsherren der BG Werl (May, Disselhoff u. Prünte) ist dadurch entstanden, dass die drei Ratsherren die Bürgergemeinschaft verlassen haben und ihre Ratsmandate dem Wählerwillen widersprechend nicht an die BG zurückgegeben, sondern zur UWG mitgenommen haben, obwohl sie nur über die Reserveliste der BG Werl überhaupt in den Stadtrat einziehen konnten. Dies war weder vom Wähler so vorgesehen noch ist es moralisch in Ordnung.\n" +
+                    "\n" +
+                    "Die erste Amtshandlung der neuen UWG-Fraktion bestand darin, einen Umbesetzungsantrag für den Planungs-, Bau- und Stadtentwicklungsausschuss zu stellen, um hiermit den sachkundigen Bürger Schmigowski aus dem Ausschuss entfernen zu können. Der sachkundige Bürger Schmigowski hat wiederholt geäußert, für eine sachlich orientierte parteiunabhängige Politik zu stehen. Eine Tatsache, die offenbar dem Willen der UWG-Fraktion im Wege stand, ihre de-facto-Koalition mit der CDU fortsetzen zu können, ohne stets der „Gefahr“ ausgesetzt zu sein, dass jemand aus der BG hier eben nicht einfach jederzeit blind folgt.\n" +
+                    "\n" +
+                    "Da dem sachkundigen Bürger Schmigowski seitens des Werler Rechtsrates in der Vergangenheit mit dem Hinweis er sei ja keine Fraktion, keine rechtlichen Beratungen erteilt wurden, wendete er sich an die Kommunalaufsicht beim Kreis Soest, nachdem er Kenntnis von dem Antrag erlangt hatte. Die Kommunalaufsicht prüfte auf seine Eingabe hin den Umbesetzungsantrag und stellte fest, dass dieser nicht den Vorgaben der Gemeindeordnung entsprach. Würde der Rat diesem Antrag zustimmen, so wäre der Beschluss rechtswidrig. Hier stellen sich direkt zwei Fragen: Hat der Bürgermeister als „Verwaltungsfachmann“ hier beim Zulassen des Antrags nicht gewusst, dass er hier einen nicht zulässigen Antrag auf die Tagesordnung der Ratssitzung setzt? Wir erinnern uns, wie der Bürgermeister beim ersten (offenbar auch schon rechtswidrigen) Antrag dieser Art öffentlich jemanden dafür kritisierte, dass er sein demokratisches Recht einer Nein-Stimme wahrgenommen hat. Die zweite sich stellende Frage ist, ob der langjährige Ratsherr und LWL-Abgeordnete May als Fraktionsvorsitzender der UWG hier auch überhaupt keine Ahnung von der Nichtzulässigkeit seines Antrages hatte. Wurde der Antrag gar nicht geprüft? Zu keiner Zeit? Von niemandem? Es wirft kein gutes Licht auf die Beteiligten, dass der sachkundige Bürger Schmigowski, der gegen seinen und gegen den Willen des Wählers ausgetauscht werden sollte, hier auf eigene Faust und mit – im Vergleich – Laienwissen ausgestattet, selbst die Initiative über die nächsthöhere Instanz ergreifen musste, um überhaupt zu erreichen, dass jemand diesen Antrag mal rechtlich fundiert hinterfragt.\n" +
+                    "\n" +
+                    "Nachdem der Kreis diese Arbeit jetzt für den Werler Bürgermeister erledigt hat, sollte man meinen, dass der Antrag von der Tagesordnung genommen wird. Bis jetzt ist das nicht geschehen. Binnen weniger als 48 Stunden bemühte man sich im Rathaus nun im Eiltempo, über die nach dem Kreis Soest folgende Instanz nach Mitteln und Wegen zu forschen, mit denen der Antrag doch aufrechterhalten werden oder so angepasst werden kann, dass trotzdem noch erreicht werden kann, den für die unabhängige BG agierenden sachkundigen Bürger Schmigowski auf schnellstmöglichem Wege aus dem Ausschuss herauszubekommen. Diese Tatkräftigkeit des Bürgermeisters wird die UWG-Fraktion sicher schwer begeistert haben. Zu keiner Zeit jedoch hat der Bürgermeister mal den Kontakt zum sachkundigen Bürger Schmigowski gesucht.  Wenn man nun die ganzen Vorgänge rund um die Umbesetzungsanträge und die Vorkommnisse in der Ratssitzung im Oktober summiert, so drängt sich hier der Gedanke an die gebotene Neutralität des Bürgermeisteramtes auf. Wir setzen voraus und gehen eigentlich davon aus, dass sich der Bürgermeister dieser Pflicht stets bewusst ist.\n" +
+                    "\n" +
+                    "Es wird für die Werlerinnen und Werler sehr interessant werden zu beobachten, ob sich die CDU diesem Treiben anschließt und beim Versuch, einen unliebsamen unabhängigen sachkundigen Bürger mit der Brechstange loszuwerden, mitmacht oder ob es sich dort durchsetzt, den kleinen „Koalitionspartner“ nach ihren Ausflügen auf rechtlich sehr dünnes Eis wieder einzufangen.\n" +
+                    "\n" +
+                    "Wir von der BG Werl jedenfalls ziehen unseren Hut und äußern unseren größten Respekt vor allen Ratsfrauen und Ratsherren, die wenn es nötig werden sollte, den Mut haben, sich in der kommenden Ratssitzung gegen dieses sehr bedenkliche Vorgehen der Umbesetzung zu stellen. Die drei Ratsherren May, Prünte und Disselhoff fordern wir hiermit zur Niederlegung ihrer Ratsmandate auf.",
+                "datum": "05.06.2023"
+            },
+            {
+                "id": 3,
+                "titel": "BG unterstützt Fahrradschutzstreifen am Hellweg",
+                "text": "Am 08.03. hat unser Vorsitzender Jens Schmigowski für die BG im Planungsausschuss für den Vorschlag der Verwaltung gestimmt, Fahrradschutzstreifen am Hellweg einzuführen.\n" +
+                    "Diese Entscheidung erfolgte nach genauer Abwägung aller Sachargumente, nach Gesprächen mit Fachleuten und vor allem im Einklang mit dem Positionspapier der BG Werl aus dem Jahr 2020.\n" +
+                    "Dieses sagt unter anderem:\n" +
+                    "\"Seit 2012 hat die BG einen Arbeitskreis „Förderung der Fahrradmobilität in Werl“ moderiert und geführt. Ergebnis ist eine Vielzahl von Fahrradweguntersuchungen und -optimierungen. Die Behinderungen (Wegesperren) auf vielen Wegen wurden zurückgeführt auf ein sinnvolles Maß, Fahrradstreifen wurden angelegt, Ampelschaltungen für den Radfahrer verbessert. Abstellmöglichkeiten in der Innenstadt, an den Bahnhöfen und an der Stadthalle wurden geschaffen. Das Fahren an den Bundesstraßen wurde sicherer gemacht. Nach einer vorliegenden Studie (Masterarbeit) gibt es noch eine Vielzahl von Stellen im Stadtgebiet, die verkehrstechnisch für den Radverkehr optimiert werden sollten. Die BG unterstützt das massiv und fordert dazu auf, mehr Geld in den Ausbau der Fahrradinfrastruktur zu geben. Da das Radfahren aber eine zukünftig deutlich höhere Bedeutung im Stadtbild haben sollte – nämlich in Bezug auf Klimaschutz, Gesundheit, Flächenverbrauch, greifen wir das Thema noch einmal für die Mittelfristplanung auf.\"\n" +
+                    "Die BG folgt damit ausdrücklich auch dem Wunsch der Mitgliederversammlung nach einer sachlich orientierten parteiunabhängigen Politik in Werl.",
+                "datum": "10.06.2023"
+            },
+            {
+                "id": 4,
+                "titel": "Wie wollen Sie den ÖPNV verbessern?",
+                "text": "Auch wir als BG-Werl beobachten eine Unzufriedenheit der Bürger mit dem ÖPNV und sehen ihn teilweise als ineffektiv an. Die Systeme Bus, Bahn und Fahrrad seien nicht genug aufeinander abgestimmt. Die BG-Werl fordert eine Analyse der gegenwärtigen Situation. Ziel muss es sein, durch eine Optimierung der Buslinien in Bezug auf Fahrtrichtung, Strecke und Auslastung insgesamt kürzere Fahrzeiten zu erreichen. Wir brauchen eine engere Taktung für wirklich nachgefragte Strecken. Eine weitere Möglichkeit wäre der langfristige Umstieg auf deutlich kleinere Busse oder ein Shuttlesystem, das auch bei aktuellem Bedarf spontan in Anspruch genommen werden kann. Außerdem fordern wir, dass die langfristige Planung des ÖPNV sich zum Ziel setzt, den Bürgern sein Angebot kostenfrei zur Verfügung zu stellen. Dies hat nicht nur eine soziale Komponente, sondern ist alleine schon aus umwelt- und klimapolitischer Sicht eine Grundvoraussetzung für eine sichere Zukunft der nächsten Generationen.",
+                "datum": "11.06.2023"
+            },
+            {
+                "id": 5,
+                "titel": "Wie wollen Sie Radeln in Werl stärken?",
+                "text": "\"WERL SATTELT UM AUF‘S RAD\"\n" +
+                    "Diesem Slogan hat sich die Bürgergemeinschaft Werl (BG Werl) verschrieben und setzt sich voll für eine nachhaltige Verbesserung der Fahrrad-Infrastruktur ein.\n" +
+                    "Wir fordern mehr und sichere Radwege, beleuchtete Straßen aus den Ortsteilen zu den Schulen, Fahrradstraßen, Abstellanlagen auch mit Lademöglichkeit für e-Bikes, sowie einen Arbeitskreis Rad unter Beteiligung von interessierten Bürgern, der Politik und der Verwaltung mit dem Ziel, 30% Radverkehr zu erreichen.\n" +
+                    "Damit klimaschädliche Emissionen reduziert werden, müssen die mit Benzin- oder Dieselkraftstoff in der Stadt zurückgelegten Kilometer gesenkt werden. Denn Radfahren ist klimafreundlich und radfahrende Menschen sind nachweislich gesünder. Wir müssen es unseren Bürgern leichter machen, das Auto stehen zu lassen und stattdessen das Fahrrad zu benutzen. Eine kostengünstige Lösung, um auch ein höherwertiges Rad zu fahren ist das Leasing über den Arbeitgeber. Hier muss mehr Aufklärungsarbeit bei Arbeitgebern und Arbeitnehmern erfolgen.",
+                "datum": "11.06.2023"
+            },
+            {
+                "id": 6,
+                "titel": "Was wollen Sie für's Werler Klima tun?",
+                "text": "Wir Menschen verbrauchen zu viel Energie, erzeugen ein Übermaß an klimaschädlichen Emissionen und tun nichts, die schädlichen Emissionen aus der Atmosphäre zu beseitigen. In der Folge läuft die Erde seit Jahrzehnten auf eine „Heißzeit“ zu. Eine Klimakatastrophe kann nur noch abgewendet werden, wenn möglichst viele Menschen daran mitwirken. Die BG-Werl bringt sich in den Kampf gegen eine weitere Erderwärmung lokal ein. Die Arbeit muss auf allen Ebenen geführt werden. Wir haben beantragt, Flächen in der Stadt aufzuforsten. Nur 4% der Flächen sind bewaldet, in anderen Städten bis zu 30%. Die Aufklärung der Bürger über das drohende Desaster muss verstärkt werden. Schon junge Menschen sollen an das Problem herangeführt werden, weshalb wir die Anlage eines Schülerwaldes fordern. Wir fordern die Rückgewinnung von Wärme aus Abwasser, die Gewinnung von Biogas aus Klärschlämmen, den Einsatz von Fernwärme. Verfahren zur technischen Eliminierung von schädlichen Klimagasen unterstützen wir. Wir fordern die Verbesserung von Rad- und Busverkehr. Mobilität kann nicht weiter durch mit Benzin und Diesel betriebene Autos stattfinden.",
+                "datum": "11.06.2023"
+            }
+        ],
+        erfolge: [
+            {
+                id: 1,
+                img: require('../assets/erfolge/ratsinfo_seitenleiste.png'),
+                text: '... auf Antrag der BG in 2012 ein Ratsinformationssystem für die Ratsmitglieder eingeführt wurde, das hilft, Tausende von Papierseiten nicht mehr auszudrucken und den Ratsmitgliedern zuzustellen, sondern welches ihnen ermöglicht, alle Unterlagen am Rechner einzusehen und zu bearbeiten. Dass dabei gleichzeitig ein digitales Archiv entsteht, ist ein gewollter Nebeneffekt, der die Arbeit des Rates transparenter und nachhaltiger machen kann.'
+            },
+            {
+                id: 2,
+                img: require('../assets/erfolge/bushaltestelle_holtum.png'),
+                text: '... auf Beharren der BG die Bushaltestelle in Holtum einen neuen Platz erhielt, somit der Busverkehr aus dem engen Kirchenumfeld geholt wurde. Erst bei einem Ortstermin konnte die BG mit ihrem Vorschlag überzeugen, der zuletzt sogar preiswerter war als der Vorschlag von CDU und SPD.'
+            },
+            {
+                id: 3,
+                img: '',
+                text: '... auf Anregung der BG eine zukunftsweisende Erdgastankstelle an der Tankstelle Rubarth geschaffen wurde?'
+            },
+            {
+                id: 4,
+                img: '',
+                text: '... auf Vermittlung der BG ein Krematorium in Werl geschaffen wurde und somit bei vielen Todesfällen ein unnötiger "Reiseverkehr" nach Hamm unterbleiben kann?'
+            },
+            {
+                id: 5,
+                img: require('../assets/erfolge/kbw.jpg'),
+                text: '... durch massiven Personeneinsatz der BG und durch massive politische Unterstützung der BG Bürgerbegehren und Bürgerentscheid zum Erhalt des öffentlichen KBW (Kanalsystem, Straßenreinigung, Müllabfuhr, Sperrmüll, etc.) stattgefunden haben. Heute sind auch die damaligen Gegner CDU und SPD dankbar dafür, vermeidet die verhinderte Privatisierung doch steigende Kosten für Bürger.'
+            },
+            {
+                id: 6,
+                img: require('../assets/erfolge/tunnel.jpg'),
+                text: '... die BG als erste politische Gruppe die Reißlinie gezogen hat, um das Tunnelbauwerk Langenwiedenweg auf 20 Jahre zu verschieben, weil sonst nur die Schuldensumme weiter erhöht würde? Andere Parteien haben sich angeschlossen und auch die CDU verlangt keine schnelle Lösung mehr. Nur die SPD verharrt in ihrer 30 Jahre alten Position, egal, wieviel Schulden den Bürgern aufgebürdet werden.'
+            },
+            {
+                id: 7,
+                img: '',
+                text: '... die BG bereits seit 2002 in ihren Haushaltsreden und ihrer Politik die Haushaltsführung angegriffen hat und in der aktuellen Legislaturperiode endlich mit der CDU und den Grünen die Wende geschafft hat?'
+            },
+            {
+                id: 8,
+                img: require('../assets/erfolge/wald.jpg'),
+                text: '... die BG sich als erste Fraktion massiv gegen den Ausbau des Werler Stadtwaldes zum Verkehrsübungsplatz gewandt hat?'
+            },
+            {
+                id: 9,
+                img: '',
+                text: '... die BG in der aktuellen, finanziell angespannten Situation auch den Mut hat, zurückzustecken und versucht, den Bürgern die Haushaltssorgen transparent zu machen?'
+            },
+            {
+                id: 10,
+                img: '',
+                text: '... die BG seit langem Kreisverkehre statt Ampeln fordert, was langsam auch in Werl Einkehr findet?'
+            },
+            {
+                id: 11,
+                img: '',
+                text: '... sich die BG sachlich und konstruktiv mit den Konzepten aller Werler Parteien auseinandersetzt und gemeinsam mit ihnen nach der jeweils bestmöglichen Lösung sucht?'
+            },
+            {
+                id: 12,
+                img: require('../assets/erfolge/gradierwerk.jpg'),
+                text: '... die Idee für das Gradierwerk im Kurpark u.v.a.m von der BG initiiert wurden?'
+            },
+            {
+                id: 13,
+                img: require('../assets/erfolge/kaemperstrasse.jpg'),
+                text: '... Gründungsmitglieder der BG Zeit und Geld geopfert haben, um eine Straße zu verhindern, die quer durch die Kernstadt (Westtangente) geführt werden sollte?'
+            },
+            {
+                id: 14,
+                img: require('../assets/erfolge/freibad.jpg'),
+                text: '... die Werler BG sich immer für ein leistungsfähiges Frei- und Hallenbad mit familienfreundlichen Preisen eingesetzt hat?'
+            },
+            {
+                id: 15,
+                img: require('../assets/erfolge/autobahnparkplatz.jpg'),
+                text: '... die BG mit dafür verantwortlich ist, dass es an der Autobahnabfahrt Büderich einen Parkplatz gibt?'
+            }
+        ]
+    },
+    getters: {
+        user: (state) => {
+            return state.user
+        }
+    },
+    mutations: {
+        user(state, user) {
+            state.user = user
+        }
+    },
+    actions: {
+        user(context, user) {
+            context.commit('user', user)
+        }
+    },
+    modules: {}
 })
--- src/views/ErfolgeView.vue
+++ src/views/ErfolgeView.vue
@@ -6,21 +6,15 @@
       <div style="background-color: rgba(255,255,255,0.56);height: 100%; width: 100%">
         <h1 class="text-center pt-5">Wusstest du, dass...</h1>
 
-        <v-virtual-scroll :item-height="30" :items="erfolge" height="65vh">
+        <v-virtual-scroll :item-height="30" :items="$store.state.erfolge" height="65vh">
           <template v-slot="{ item }">
             <div class="d-flex justify-center">
               <v-card class="mt-3 card">
-                <v-row v-if="item.img !=''">
+                <v-row>
                   <v-col cols="2">
-                    <v-img :src="item.img" width="200"/>
+                    <v-img v-if="item.img !=''" :src="item.img" width="200"/>
                   </v-col>
                   <v-col class="d-flex align-center" cols="10">
-                    <p class="text-center mx-2 my-2">{{ item.text }}</p>
-                  </v-col>
-                </v-row>
-
-                <v-row v-else class="d-flex justify-center">
-                  <v-col cols="10">
                     <p class="text-center mx-2 my-2">{{ item.text }}</p>
                   </v-col>
                 </v-row>
@@ -42,83 +36,6 @@ export default {
     return {
       name: "Erfolge",
       background: require('../assets/erfolge.png'),
-      erfolge: [
-        {
-          id: 1,
-          img: require('../assets/erfolge/ratsinfo_seitenleiste.png'),
-          text: '... auf Antrag der BG in 2012 ein Ratsinformationssystem für die Ratsmitglieder eingeführt wurde, das hilft, Tausende von Papierseiten nicht mehr auszudrucken und den Ratsmitgliedern zuzustellen, sondern welches ihnen ermöglicht, alle Unterlagen am Rechner einzusehen und zu bearbeiten. Dass dabei gleichzeitig ein digitales Archiv entsteht, ist ein gewollter Nebeneffekt, der die Arbeit des Rates transparenter und nachhaltiger machen kann.'
-        },
-        {
-          id: 2,
-          img: require('../assets/erfolge/bushaltestelle_holtum.png'),
-          text: '... auf Beharren der BG die Bushaltestelle in Holtum einen neuen Platz erhielt, somit der Busverkehr aus dem engen Kirchenumfeld geholt wurde. Erst bei einem Ortstermin konnte die BG mit ihrem Vorschlag überzeugen, der zuletzt sogar preiswerter war als der Vorschlag von CDU und SPD.'
-        },
-        {
-          id: 3,
-          img: '',
-          text: '... auf Anregung der BG eine zukunftsweisende Erdgastankstelle an der Tankstelle Rubarth geschaffen wurde?'
-        },
-        {
-          id: 4,
-          img: '',
-          text: '... auf Vermittlung der BG ein Krematorium in Werl geschaffen wurde und somit bei vielen Todesfällen ein unnötiger "Reiseverkehr" nach Hamm unterbleiben kann?'
-        },
-        {
-          id: 5,
-          img: require('../assets/erfolge/kbw.jpg'),
-          text: '... durch massiven Personeneinsatz der BG und durch massive politische Unterstützung der BG Bürgerbegehren und Bürgerentscheid zum Erhalt des öffentlichen KBW (Kanalsystem, Straßenreinigung, Müllabfuhr, Sperrmüll, etc.) stattgefunden haben. Heute sind auch die damaligen Gegner CDU und SPD dankbar dafür, vermeidet die verhinderte Privatisierung doch steigende Kosten für Bürger.'
-        },
-        {
-          id: 6,
-          img: require('../assets/erfolge/tunnel.jpg'),
-          text: '... die BG als erste politische Gruppe die Reißlinie gezogen hat, um das Tunnelbauwerk Langenwiedenweg auf 20 Jahre zu verschieben, weil sonst nur die Schuldensumme weiter erhöht würde? Andere Parteien haben sich angeschlossen und auch die CDU verlangt keine schnelle Lösung mehr. Nur die SPD verharrt in ihrer 30 Jahre alten Position, egal, wieviel Schulden den Bürgern aufgebürdet werden.'
-        },
-        {
-          id: 7,
-          img: '',
-          text: '... die BG bereits seit 2002 in ihren Haushaltsreden und ihrer Politik die Haushaltsführung angegriffen hat und in der aktuellen Legislaturperiode endlich mit der CDU und den Grünen die Wende geschafft hat?'
-        },
-        {
-          id: 8,
-          img: require('../assets/erfolge/wald.jpg'),
-          text: '... die BG sich als erste Fraktion massiv gegen den Ausbau des Werler Stadtwaldes zum Verkehrsübungsplatz gewandt hat?'
-        },
-        {
-          id: 9,
-          img: '',
-          text: '... die BG in der aktuellen, finanziell angespannten Situation auch den Mut hat, zurückzustecken und versucht, den Bürgern die Haushaltssorgen transparent zu machen?'
-        },
-        {
-          id: 10,
-          img: '',
-          text: '... die BG seit langem Kreisverkehre statt Ampeln fordert, was langsam auch in Werl Einkehr findet?'
-        },
-        {
-          id: 11,
-          img: '',
-          text: '... sich die BG sachlich und konstruktiv mit den Konzepten aller Werler Parteien auseinandersetzt und gemeinsam mit ihnen nach der jeweils bestmöglichen Lösung sucht?'
-        },
-        {
-          id: 12,
-          img: require('../assets/erfolge/gradierwerk.jpg'),
-          text: '... die Idee für das Gradierwerk im Kurpark u.v.a.m von der BG initiiert wurden?'
-        },
-        {
-          id: 13,
-          img: require('../assets/erfolge/kaemperstrasse.jpg'),
-          text: '... Gründungsmitglieder der BG Zeit und Geld geopfert haben, um eine Straße zu verhindern, die quer durch die Kernstadt (Westtangente) geführt werden sollte?'
-        },
-        {
-          id: 14,
-          img: require('../assets/erfolge/freibad.jpg'),
-          text: '... die Werler BG sich immer für ein leistungsfähiges Frei- und Hallenbad mit familienfreundlichen Preisen eingesetzt hat?'
-        },
-        {
-          id: 15,
-          img: require('../assets/erfolge/autobahnparkplatz.jpg'),
-          text: '... die BG mit dafür verantwortlich ist, dass es an der Autobahnabfahrt Büderich einen Parkplatz gibt?'
-        }
-      ]
 
 
     }
