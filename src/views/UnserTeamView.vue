5be4c8a7701c5c8ba3bf185d31aacb8fe83b5894
Stefan <stefan.franke@itc-studenten.de>
2023-06-15 11:25:35 +0200

    AdminView auslagerung

--- src/views/UnserTeamView.vue
+++ src/views/UnserTeamView.vue
@@ -4,35 +4,27 @@
     <HeaderComponent/>
       <div class="d-flex justify-center" style="background-color: rgba(255,255,255,0);height: 100%; width: 100%">
         <v-sheet
-            style="position: fixed;bottom: 20px;width: 100%; height: 100%;"
+            style="position: fixed;bottom: 4%;width: 100%; height: 100%;"
             class="bg-transparent"
             elevation="8"
 
         >
           <v-slide-group
               v-model="model"
-              class="pa-4"
-              style="margin-top: 22%"
-              selected-class="test"
-              multiple
-              show-arrows
+              style="height: 100%"
+              class="slide-group pa-4 d-flex align-end"
           >
             <v-slide-group-item
                 v-for="n in $store.state.vorstand"
                 :key="n"
-
             >
-
               <v-card
-
                   color="grey-lighten-1"
-                  class="ma-4"
+                  class="ma-4 slide-card"
                   height="300"
                   width="200"
               >
-
                 <div class="d-flex fill-height align-center justify-center">
-
                 </div>
               </v-card>
             </v-slide-group-item>
@@ -64,5 +56,30 @@ export default {
 </script>
 
 <style scoped>
+.v-slide-group__wrapper {
+  overflow-x: auto; /* Enables the horizontal scrollbar */
+  /* Next lines hides scrollbar in different browsers for styling purposes */
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+.v-slide-group__wrapper::-webkit-scrollbar {
+  display: none; /* Chrome opera and Safary */
+  width: 0px;
+  background: transparent;
+}
+.slide-group > :first-child,
+.slide-group > :last-child {
+  margin-top: auto;
+  margin-bottom: auto;
+}
+.slide-group > :first-child >:first-child >:first-child,
+.slide-group > :last-child >:first-child >:first-child{
+min-width: 80px;
+  min-height: 80px;
+}
+.slide-group > :first-child >:first-child ,
+.slide-group > :last-child >:first-child {
+  font-size: 80px;
+}
 
 </style>
