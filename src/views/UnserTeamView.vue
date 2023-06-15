2f48496d9aa45c776b23ea2640b4d59ca48290cc
Stefan <stefan.franke@itc-studenten.de>
2023-06-15 10:33:18 +0200

    AdminView auslagerung

--- src/views/UnserTeamView.vue
+++ src/views/UnserTeamView.vue
@@ -1,10 +1,43 @@
 <template>
   <div>
+    <v-img  :src="background" cover style="height: 100vh">
     <HeaderComponent/>
-    <v-img :src="background" cover style="height: 86vh">
-      <div style="background-color: rgba(255,255,255,0.56);height: 100%; width: 100%">
+      <div class="d-flex justify-center" style="background-color: rgba(255,255,255,0);height: 100%; width: 100%">
+        <v-sheet
+            style="position: fixed;bottom: 20px;width: 100%; height: 100%;"
+            class="bg-transparent"
+            elevation="8"
 
+        >
+          <v-slide-group
+              v-model="model"
+              class="pa-4"
+              style="margin-top: 22%"
+              selected-class="test"
+              multiple
+              show-arrows
+          >
+            <v-slide-group-item
+                v-for="n in $store.state.vorstand"
+                :key="n"
 
+            >
+
+              <v-card
+
+                  color="grey-lighten-1"
+                  class="ma-4"
+                  height="300"
+                  width="200"
+              >
+
+                <div class="d-flex fill-height align-center justify-center">
+
+                </div>
+              </v-card>
+            </v-slide-group-item>
+          </v-slide-group>
+        </v-sheet>
       </div>
     </v-img>
   </div>
@@ -17,8 +50,8 @@ export default {
   data() {
     return {
       name: "Unser Team",
-      background: require('../assets/VorstandUndRat.png')
-
+      background: require('../assets/VorstandUndRat.png'),
+      model: [],
     }
   },
   components: {
