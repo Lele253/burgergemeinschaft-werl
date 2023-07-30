3f40418eba0f6476b6af03904f7c7e95092080de
Stefan <stefan.franke@itc-studenten.de>
2023-07-30 23:16:15 +0200

    schönheits ausbesserungen und css entfernung

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,7 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     devServer: {
-        https: {
-            key: require('fs').readFileSync('./leandro-graf_de.key'),
-            cert: require('fs').readFileSync('./leandro-graf_de.crt'),
-        },
+
     },
     /*publicPath: process.env.NODE_ENV === 'production'
         ? '/bg-werl/'
