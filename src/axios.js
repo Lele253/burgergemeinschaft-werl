6c46abc74897a1ca19e2e3958b08f7ad88c31d16
Stefan <stefan.franke@itc-studenten.de>
2023-07-30 14:53:23 +0200

    schönheits ausbesserungen und css entfernung

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://212.227.51.43:8080/auth'
+axios.defaults.baseURL = 'https://bg-werl.site:8080/auth'
 axios.defaults.headers.common['Authorization'] = 'Bearer' + localStorage.getItem('token')
